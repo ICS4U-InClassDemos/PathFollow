88310bec2fcab7014629c9f64a8354e682caf39f
Tony Theodoropoulos <theodot@outlook.com>
2020-06-11 10:00:09 -0400

    Add project files.

---
+++ PathFollow/Enemy.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathFollow
+{
+    public class Enemy
+    {
+
+        public int x, y, size, speed;
+        string direction;
+
+        public Enemy(int _x, int _y, int _size, int _speed)
+        {
+            x = _x;
+            y = _y;
+            size = _size;
+            speed = _speed;
+
+            direction = "down";
+        }
+
+        public void Move()
+        {
+            if (direction == "down")
+            {
+                y += speed;
+            }
+            else if (direction == "right")
+            {
+                x += speed;
+            }
+            else if (direction == "up")
+            {
+                y -= speed;
+            }
+            else if (direction == "left")
+            {
+                x -= speed;
+            }
+
+            if (x == 50 && y == 400)
+            {
+                direction = "right";
+            }
+
+            if (x == 200 && y == 400)
+            {
+                direction = "up";
+            }
+
+            if (x == 200 && y == 200)
+            {
+                direction = "right";
+            }
+        }
+
+    }
+}
---
+++ PathFollow/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PathFollow
+{
+    public partial class Form1 : Form
+    {
+        List<Enemy> enemies = new List<Enemy>();
+        SolidBrush enemyBrush = new SolidBrush(Color.Red);
+
+        int counter = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Enemy enemy = new Enemy(50, 0, 20, 5);
+            enemies.Add(enemy);
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            counter++;
+
+            if (counter == 40)
+            {
+                Enemy enemy = new Enemy(50, 0, 20, 5);
+                enemies.Add(enemy);
+                counter = 0;
+            }
+
+            foreach(Enemy enemy in enemies)
+            {
+                enemy.Move();
+            }
+
+            Refresh();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach(Enemy enemy in enemies)
+            {
+                e.Graphics.FillRectangle(enemyBrush, enemy.x, enemy.y, enemy.size, enemy.size);
+            }
+        }
+    }
+}
